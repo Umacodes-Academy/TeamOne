9c6dac5ffa62b4bfb647feb4c0bcddd67df3b436
SalmanTahir7 <tahirsalman7@gmail.com>
2024-02-19 19:08:52 -0500

    Connected to DB

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const shortid = require('shortid');
+require('dotenv').config();
+const app = express();
+
+app.use(express());
+app.use(cors());
+app.use(express.json());
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser:true
+        });
+        console.log('Connected to the DB');
+    } catch (err){
+        console.log('NOT CONNECTED' + err);
+    }
+}
+
+connectDB();
+
+app.listen('4000', () => {
+    console.log('listening on 4000');
+})
